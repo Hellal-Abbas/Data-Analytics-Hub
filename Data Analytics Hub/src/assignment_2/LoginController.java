0a3a3c830d376e3b58f192045f8823d6af475dc5
Hellal-Abbas <111841589+Hellal-Abbas@users.noreply.github.com>
2023-10-03 17:00:08 +1100

    Update LoginController.java

--- Data Analytics Hub/src/assignment_2/LoginController.java
+++ Data Analytics Hub/src/assignment_2/LoginController.java
@@ -1,5 +1,24 @@
 package assignment_2;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
+public class LoginController {
+	
+	@FXML
+	private TextField usernameField;
+	
+	@FXML
+	private TextField passwordField;
+
+	@FXML
+	public void loginHandler(ActionEvent e) {
+		
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+		
+		
+	}
+	
 }
