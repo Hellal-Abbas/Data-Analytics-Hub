0fd9a5fec9f430236599365bb986023fb817e23c
Hellal-Abbas <111841589+Hellal-Abbas@users.noreply.github.com>
2023-10-06 17:02:11 +1100

    updates to data analytics hub menu

--- Data Analytics Hub/src/DataAnalyticsHubController.java
+++ Data Analytics Hub/src/DataAnalyticsHubController.java
@@ -9,8 +9,14 @@ import javafx.scene.control.Label;
 
 public class DataAnalyticsHubController implements Initializable {
 	
+	private String firstname;
+	
+	private String lastname;
+	
 	private String username;
 	
+	private String password;
+
 	DataSingleton dataSingleton = DataSingleton.getInstance();
 	
 	@FXML
@@ -18,12 +24,29 @@ public class DataAnalyticsHubController implements Initializable {
 	
 	@Override
 	public void initialize(URL url, ResourceBundle resourcebundle) {
+		
 		username = dataSingleton.getUsername();
+		
+		firstname = userDetails.getUserDetails(username, "firstname");
+		lastname = userDetails.getUserDetails(username, "lastname");
+		password = userDetails.getUserDetails(username, "password");
+
+		firstname = firstname.substring(0, 1).toUpperCase() + firstname.substring(1);
+		lastname = lastname.substring(0, 1).toUpperCase()  + lastname.substring(1);
+
+		welcomeOutput.setText("Welcome " + firstname + " " + lastname + ".");
 	}
 	
 	@FXML
 	public void showDashBoardHandler(ActionEvent event) throws IOException {		
-		welcomeOutput.setText("Welcome " + username + "!");
+	}
+	
+	@FXML
+	public void accountHandler(ActionEvent event) throws IOException {		
+	}
+	
+	@FXML
+	public void logoutHandler(ActionEvent event) throws IOException {		
 	}
 	
 }
---
+++ Data Analytics Hub/src/userDetails.java
@@ -0,0 +1,32 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class userDetails {
+	
+	public static String getUserDetails(String username, String UserInformation) {
+		
+		final String TABLE_NAME = "Users";
+		String userInfo = null;
+
+		try (Connection con = DatabaseConnection.getConnection();
+				Statement stmt = con.createStatement();) {
+			
+			
+			String query = "SELECT " + UserInformation + " FROM " + TABLE_NAME + " WHERE Username = " +
+					String.format("'%s'", username);
+
+			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				while(resultSet.next()) {
+					userInfo = resultSet.getString(UserInformation);
+				}
+			} 
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		return userInfo;
+	}
+
+}
