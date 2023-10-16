eba9b354e7ab41038ca289ff8dbd2daba83563dd
Hellal-Abbas <111841589+Hellal-Abbas@users.noreply.github.com>
2023-10-16 13:53:29 +1100

    Update WelcomeController.java

--- Data Analytics Hub/src/WelcomeController.java
+++ Data Analytics Hub/src/WelcomeController.java
@@ -6,40 +6,33 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class WelcomeController {
-	
 
-	@FXML
-	public void GoToSignUpHandler(ActionEvent event) throws IOException {
-		
+    @FXML
+    public void GoToSignUpHandler(ActionEvent event) throws IOException {
+
         SignupScene signup = new SignupScene();
-		Stage stage = new Stage();
-		stage.setTitle(signup.getTitle());
-		stage.setScene(signup.getScene());
-		stage.show();
-		
-		
-		// Hide this current window (if this is what you want)
-		((Node)(event.getSource())).getScene().getWindow().hide();
-		
-		
-	}
-	
-	
-	@FXML
-	public void GoToLoginHandler(ActionEvent event) throws IOException {
-		
-        LoginScene login = new LoginScene();
-		Stage stage = new Stage();
-		stage.setTitle(login.getTitle());
-		stage.setScene(login.getScene());
-		stage.show();
-		
-		
-		// Hide this current window (if this is what you want)
-		((Node)(event.getSource())).getScene().getWindow().hide();
+        Stage stage = new Stage();
+        stage.setTitle(signup.getTitle());
+        stage.setScene(signup.getScene());
+        stage.show();
 
-	}
-	
-	
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+
+    }
+
+    @FXML
+    public void GoToLoginHandler(ActionEvent event) throws IOException {
+
+        LoginScene login = new LoginScene();
+        Stage stage = new Stage();
+        stage.setTitle(login.getTitle());
+        stage.setScene(login.getScene());
+        stage.show();
+
+        // Hide this current window (if this is what you want)
+        ((Node)(event.getSource())).getScene().getWindow().hide();
+
+    }
 
 }
