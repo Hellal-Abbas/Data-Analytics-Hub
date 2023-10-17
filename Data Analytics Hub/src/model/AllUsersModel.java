027a8579c4f6c2bfd21e0b7ee12623a165ef5fda
Hellal-Abbas <111841589+Hellal-Abbas@users.noreply.github.com>
2023-10-17 21:28:14 +1100

    Added comments to Model classes

--- Data Analytics Hub/src/model/AllUsersModel.java
+++ Data Analytics Hub/src/model/AllUsersModel.java
@@ -1,17 +1,21 @@
 package model;
 
+//This class represents a model for storing all username information.
 public class AllUsersModel {
 
 	String username;
 
+	// Constructor for initializing the AllUsersModel object with a username.
 	public AllUsersModel(String username) {
 		this.username = username;
 	}
 
+	// Getter method to retrieve the username of the user.
 	public String getUsername() {
 		return username;
 	}
 
+	// Setter method to update the username with a new value.
 	public void setUsername(String datetime) {
 		this.username = datetime;
 	}
--- Data Analytics Hub/src/model/PostsTableModel.java
+++ Data Analytics Hub/src/model/PostsTableModel.java
@@ -1,11 +1,14 @@
 package model;
 
+//This class represents a model for storing post-related information.
 public class PostsTableModel {
 
 	int postid, likes, shares;
 
 	String content, author, datetime, username;
 
+	// Constructor for initializing the PostsTableModel object with post-related
+	// information.
 	public PostsTableModel(int postid, String content, String author, int likes, int shares, String datetime,
 			String username) {
 		this.postid = postid;
@@ -17,58 +20,72 @@ public class PostsTableModel {
 		this.username = username;
 	}
 
+	// Getter method to retrieve the post ID.
 	public int getPostid() {
 		return postid;
 	}
 
+	// Setter method to update the post ID with a new value.
 	public void setPostid(int postid) {
 		this.postid = postid;
 	}
 
+	// Getter method to retrieve the content.
 	public String getContent() {
 		return content;
 	}
 
+	// Setter method to update the content with a new value.
 	public void setContent(String content) {
 		this.content = content;
 	}
 
+	// Getter method to retrieve the author.
 	public String getAuthor() {
 		return author;
 	}
 
+	// Setter method to update the author with a new value.
 	public void setAuthor(String author) {
 		this.author = author;
 	}
 
+	// Getter method to retrieve the likes.
 	public int getLikes() {
 		return likes;
 	}
 
+	// Setter method to update the likes with a new value.
 	public void setLikes(int likes) {
 		this.likes = likes;
 	}
 
+	// Getter method to retrieve the shares.
 	public int getShares() {
 		return shares;
 	}
 
+	// Setter method to update the shares with a new value.
 	public void setShares(int shares) {
 		this.shares = shares;
 	}
 
+	// Getter method to retrieve the date time.
 	public String getDatetime() {
 		return datetime;
 	}
 
+	// Setter method to update the date time with a new value.
 	public void setDatetime(String datetime) {
 		this.datetime = datetime;
 	}
 
+	// Getter method to retrieve the username.
 	public String getUsername() {
 		return username;
 	}
 
+	// Setter method to update the username with a new value.
 	public void setUsername(String username) {
 		this.username = username;
 	}
