ea46f6acf0eefcd0b5c6965fc631eaa980e691e8
Hellal-Abbas <111841589+Hellal-Abbas@users.noreply.github.com>
2023-10-18 09:57:34 +1100

    Added comments to helper classes

--- Data Analytics Hub/src/helpers/TopLikedPostsQuery.java
+++ Data Analytics Hub/src/helpers/TopLikedPostsQuery.java
@@ -11,17 +11,22 @@ import javafx.collections.ObservableList;
 import model.AllUsersModel;
 import model.PostsTableModel;
 
+//This class provides methods for querying and retrieving top liked posts and user data from the database.
 public class TopLikedPostsQuery {
 
+	// Method to retrieve usernames of all users who have posted.
 	public static ObservableList<AllUsersModel> AllUsers(String username) {
-
+		// Creating an empty ObservableList to store usernames.
 		ObservableList<AllUsersModel> listview = FXCollections.observableArrayList();
+		// Defining the table name in the database.
 		final String TABLE_NAME = "SocialMediaPosts";
 
 		try (Connection con = DatabaseConnection.getConnection(); Statement stmt = con.createStatement();) {
+			// SQL query to select distinct usernames from the 'SocialMediaPosts' table.
 			String query = "SELECT DISTINCT username FROM " + TABLE_NAME;
 
 			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				// Iterating through the result set and adding usernames to the ObservableList.
 				while (resultSet.next()) {
 					listview.add(new AllUsersModel(resultSet.getString("username")));
 				}
@@ -29,18 +34,25 @@ public class TopLikedPostsQuery {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+		// Returning the populated ObservableList of usernames.
 		return listview;
 	}
 
+	// Method to retrieve all posts sorted by the number of likes in descending
+	// order.
 	public static ObservableList<PostsTableModel> AllPosts(String username) {
-
+		// Creating an empty ObservableList to store posts data.
 		ObservableList<PostsTableModel> listview = FXCollections.observableArrayList();
+		// Defining the table name in the database.
 		final String TABLE_NAME = "SocialMediaPosts";
 
 		try (Connection con = DatabaseConnection.getConnection(); Statement stmt = con.createStatement();) {
+			// SQL query to select all posts and order them by the number of likes in
+			// descending order.
 			String query = "SELECT * FROM " + TABLE_NAME + " ORDER BY Likes DESC";
 
 			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				// Iterating through the result set and adding posts data to the ObservableList.
 				while (resultSet.next()) {
 					listview.add(new PostsTableModel(resultSet.getInt("postID"), resultSet.getString("Content"),
 							resultSet.getString("Author"), resultSet.getInt("Likes"), resultSet.getInt("Shares"),
@@ -50,19 +62,25 @@ public class TopLikedPostsQuery {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+		// Returning the populated ObservableList of posts data.
 		return listview;
 	}
 
+	// Method to retrieve a specific number of top liked posts.
 	public static ObservableList<PostsTableModel> NumOfPosts(int numOfPosts) {
-
+		// Creating an empty ObservableList to store posts data.
 		ObservableList<PostsTableModel> listview = FXCollections.observableArrayList();
+		// Defining the table name in the database.
 		final String TABLE_NAME = "SocialMediaPosts";
 
 		try (Connection con = DatabaseConnection.getConnection(); Statement stmt = con.createStatement();) {
+			// SQL query to select a specific number of top liked posts and order them by
+			// the number of likes in descending order.
 			String query = "SELECT * FROM " + TABLE_NAME + " ORDER BY Likes DESC" + " LIMIT "
 					+ String.format("%s", numOfPosts);
 
 			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				// Iterating through the result set and adding posts data to the ObservableList.
 				while (resultSet.next()) {
 					listview.add(new PostsTableModel(resultSet.getInt("postID"), resultSet.getString("Content"),
 							resultSet.getString("Author"), resultSet.getInt("Likes"), resultSet.getInt("Shares"),
@@ -72,19 +90,25 @@ public class TopLikedPostsQuery {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+		// Returning the populated ObservableList of posts data.
 		return listview;
 	}
 
+	// Method to retrieve top liked posts posted by a specific user.
 	public static ObservableList<PostsTableModel> Usernameonly(String username) {
-
+		// Creating an empty ObservableList to store posts data.
 		ObservableList<PostsTableModel> listview = FXCollections.observableArrayList();
+		// Defining the table name in the database.
 		final String TABLE_NAME = "SocialMediaPosts";
 
 		try (Connection con = DatabaseConnection.getConnection(); Statement stmt = con.createStatement();) {
+			// SQL query to select top liked posts by a specific user and order them by the
+			// number of likes in descending order.
 			String query = "SELECT * FROM " + TABLE_NAME + " WHERE username = " + String.format("'%s'", username)
 					+ " ORDER BY Likes DESC";
 
 			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				// Iterating through the result set and adding posts data to the ObservableList.
 				while (resultSet.next()) {
 					listview.add(new PostsTableModel(resultSet.getInt("postID"), resultSet.getString("Content"),
 							resultSet.getString("Author"), resultSet.getInt("Likes"), resultSet.getInt("Shares"),
@@ -94,19 +118,26 @@ public class TopLikedPostsQuery {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+		// Returning the populated ObservableList of posts data.
 		return listview;
 	}
 
+	// Method to retrieve a specific number of top liked posts posted by a specific
+	// user.
 	public static ObservableList<PostsTableModel> NumOfPostsWithUsername(String username, int numOfPosts) {
-
+		// Creating an empty ObservableList to store posts data.
 		ObservableList<PostsTableModel> listview = FXCollections.observableArrayList();
+		// Defining the table name in the database.
 		final String TABLE_NAME = "SocialMediaPosts";
 
 		try (Connection con = DatabaseConnection.getConnection(); Statement stmt = con.createStatement();) {
+			// SQL query to select a specific number of top liked posts by a specific user
+			// and order them by the number of likes in descending order.
 			String query = "SELECT * FROM " + TABLE_NAME + " WHERE username = " + String.format("'%s'", username)
 					+ " ORDER BY Likes DESC" + " LIMIT " + String.format("%s", numOfPosts);
 
 			try (ResultSet resultSet = stmt.executeQuery(query)) {
+				// Iterating through the result set and adding posts data to the ObservableList.
 				while (resultSet.next()) {
 					listview.add(new PostsTableModel(resultSet.getInt("postID"), resultSet.getString("Content"),
 							resultSet.getString("Author"), resultSet.getInt("Likes"), resultSet.getInt("Shares"),
@@ -116,6 +147,7 @@ public class TopLikedPostsQuery {
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
+		// Returning the populated ObservableList of posts data.
 		return listview;
 	}
 }
